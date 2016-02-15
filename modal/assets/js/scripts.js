a27583b0ef6df959bbfa1be09748bf1287b8d0b9
Dennis Goldin <dennis.goldin@gmail.com>
2016-02-15 13:41:25 -0500

    Add files and directory for textarea. Add solution to modal assignment.

---
+++ modal/assets/js/scripts.js
@@ -0,0 +1,17 @@
+$(function () {
+  'use strict';
+
+  $('.btn').on('click', function () {
+    $(this).text('Hi'); /* this --> only the button being clicked */
+    $('.modal-container').addClass('showing');
+  });
+
+  $('.modal-close, .modal-container').on('click', function () {
+    $('.modal-container').removeClass('showing');
+    $(".btn").text('Click me');  /*  all buttons with class ".btn" */
+  });
+
+  $('.modal').click(function (e) {
+    e.stopPropagation();
+  });
+});
